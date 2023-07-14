a2b3c6a191fdaf8b7326af3394a02520b7cf4639
Valerio-W <valerio.walbert@outlook.com>
2023-07-14 13:24:55 +0200

    In the Client project, implement the create method to access to the API Project

--- MyPresentLists_Client/src/main/java/be/walbert/DAO/Presents_ListDAO.java
+++ MyPresentLists_Client/src/main/java/be/walbert/DAO/Presents_ListDAO.java
@@ -2,15 +2,43 @@ package be.walbert.DAO;
 
 import java.util.ArrayList;
 
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
+
 import be.walbert.javabeans.Presents_List;
 
 public class Presents_ListDAO extends DAO <Presents_List>{
 
 	@Override
 	public boolean create(Presents_List obj) {
-		// TODO Auto-generated method stub
-		return false;
+		//Convert Object to json
+		ObjectMapper objectMapper = new ObjectMapper();
+
+	    try {
+	        // Send POST request with JSON
+			String json = objectMapper.writeValueAsString(obj);
+	        ClientResponse res = this.ressource
+	                .path("presents_list/create")
+	                .accept(MediaType.APPLICATION_JSON)
+	                .type(MediaType.APPLICATION_JSON)
+	                .post(ClientResponse.class, json);
+
+	        if (res.getStatus() == 201) {
+	            return true;
+	        }
+	    } catch (Exception ex) {
+	        System.out.println(ex.getMessage());
+	        return false;
+	    }
+	    return false;
 	}
+
+
+
 
 	@Override
 	public boolean delete(Presents_List obj) {
