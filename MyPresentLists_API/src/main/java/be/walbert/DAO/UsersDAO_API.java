f9716be6fc5488e36c37b9d039680c8abb709807
Valerio-W <valerio.walbert@outlook.com>
2023-07-15 12:59:48 +0200

    Modify GetUser method in API project to get the presents_lis of user

--- MyPresentLists_API/src/main/java/be/walbert/DAO/UsersDAO_API.java
+++ MyPresentLists_API/src/main/java/be/walbert/DAO/UsersDAO_API.java
@@ -4,12 +4,16 @@ import java.math.BigDecimal;
 import java.sql.Array;
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Struct;
+import java.sql.Timestamp;
 import java.sql.Types;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
+import be.walbert.Javabeans.Presents_List_API;
 import be.walbert.Javabeans.Users_API;
 import oracle.jdbc.OracleTypes;
 
@@ -88,29 +92,51 @@ public class UsersDAO_API extends DAO<Users_API>  {
 	public Users_API GetUser(String pseudo, String password) {
 		try {
 
-			CallableStatement callableStatement = connect.prepareCall("{call Get_User(?, ?, ?)}");
-		       			
-		    callableStatement.setString(1, pseudo);
-		    callableStatement.setString(2, password);
+			CallableStatement callableStatement_Users = connect.prepareCall("{call Get_User(?, ?, ?)}");
+	        CallableStatement callableStatement_PresentsList = connect.prepareCall("{call GetListsByUser(?, ?)}");
+	        
+	        callableStatement_Users.setString(1, pseudo);
+	        callableStatement_Users.setString(2, password);
+	        callableStatement_Users.registerOutParameter(3, OracleTypes.CURSOR);
+	        callableStatement_Users.execute();
 
-		    callableStatement.registerOutParameter(3, OracleTypes.CURSOR);
-
-		    callableStatement.execute();
-
-	        ResultSet rs = (ResultSet) callableStatement.getObject(3);
+	        ResultSet rs = (ResultSet) callableStatement_Users.getObject(3);
 
 	        if (rs.next()) {
 	        	int users_id = rs.getInt("ID_USERS");
 	            String email = rs.getString("EMAIL");
 	            Users_API user = new Users_API(users_id, pseudo, password, email);
 	            
-	            return user;
+	            callableStatement_PresentsList.setInt(1, users_id);
+	            callableStatement_PresentsList.registerOutParameter(2, OracleTypes.ARRAY, "LISTTABLE");
+	            callableStatement_PresentsList.execute();
 
-	        } 
+	            Array array = callableStatement_PresentsList.getArray(2);
+	            Object[] dataArray = (Object[]) array.getArray();
+
+	            for (Object data : dataArray) {
+	                Object[] attributes = ((Struct) data).getAttributes();
+	                
+	                int id_list = ((BigDecimal) attributes[0]).intValue();
+	                LocalDate limit_date = ((Timestamp) attributes[1]).toLocalDateTime().toLocalDate();
+	                String occasion = (String) attributes[2];
+	                int state = ((BigDecimal) attributes[3]).intValue();
+	                
+	                boolean stateValue = (state == 1);
+	                Presents_List_API presentsList = new Presents_List_API(id_list, limit_date, occasion, stateValue, user);
+	                user.addList(presentsList);
+	            }
+
+	            rs.close();
+	            callableStatement_Users.close();
+	            callableStatement_PresentsList.close();
+	            
+	            return user;
+	        }
 	        rs.close();
-	        callableStatement.close();
+	        callableStatement_Users.close();
 	    } catch (SQLException e) {
-	        System.out.println("erreur de oracle");
+	        System.out.println("oracle error");
 	    }
 		return null;
 	}
