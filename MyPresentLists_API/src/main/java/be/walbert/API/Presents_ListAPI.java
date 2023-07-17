f1a035ed8c185c7a545872eeb57d053e8f053b34
Valerio-W <valerio.walbert@outlook.com>
2023-07-17 22:40:28 +0200

    Implement presents_list/all endpoint in the Presents_ListAPI to get all lists

--- MyPresentLists_API/src/main/java/be/walbert/API/Presents_ListAPI.java
+++ MyPresentLists_API/src/main/java/be/walbert/API/Presents_ListAPI.java
@@ -2,9 +2,11 @@ package be.walbert.API;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.ArrayList;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -23,7 +25,7 @@ public class Presents_ListAPI {
 	@Path("/create")
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response createUsers(String json) {
+	public Response create_PresentsList(String json) {
 	    if (json == null) {
 	        return Response.status(Status.BAD_REQUEST).build();
 	    }
@@ -77,4 +79,63 @@ public class Presents_ListAPI {
 		}
 		return Response.status(Status.OK).entity(presents_list).build();
 	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/all")
+	public Response find_allPresentsLists() {
+		ArrayList<Presents_List_API> allLists = new ArrayList<>();
+		 
+		try {
+	        allLists = Presents_List_API.findAll(); 
+
+			if (allLists == null) {
+				return Response.status(Status.SERVICE_UNAVAILABLE).build();
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return Response.status(Status.OK).entity(allLists).build();
+	}
+	
+	@Path("/update")
+	@PUT
+	@Produces(MediaType.APPLICATION_JSON)
+ 	public Response updatePresentsList(String json) {
+	    if (json == null) {
+	        return Response.status(Status.BAD_REQUEST).build();
+	    }
+
+	    try {
+	        JSONObject jsonObject = new JSONObject(json);
+
+	        int idList = jsonObject.getInt("id_list");
+	        String occasion = jsonObject.getString("occasion");
+	        boolean state = jsonObject.getBoolean("state");
+	        JSONObject limitDateObject = jsonObject.getJSONObject("limit_date");
+	        int year = limitDateObject.getInt("year");
+	        String monthString = limitDateObject.getString("month");
+	        int day = limitDateObject.getInt("dayOfMonth");
+	        
+	        Month month = Month.valueOf(monthString.toUpperCase());  
+	        LocalDate limitDate = LocalDate.of(year, month, day);
+	        
+	        JSONObject userObject = jsonObject.getJSONObject("owner");
+	        Users_API user = new Users_API();
+	        user.setId(userObject.getInt("id_users"));
+	        
+	        Presents_List_API presentsList = new Presents_List_API(idList, limitDate, occasion, state, user);
+	        if (!presentsList.update()) {
+	            return Response.status(Status.SERVICE_UNAVAILABLE).build();
+	        } else {
+	            return Response.status(Status.OK).build();
+	        }
+	    } catch (Exception ex) {
+	        return Response.status(Status.BAD_REQUEST).build();
+	    }
+	}
+
+	
+	
 }
