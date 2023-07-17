83b170c4afdd5ee3349315d33885af0871ba8a1e
Valerio-W <valerio.walbert@outlook.com>
2023-07-17 13:42:25 +0200

    Create the Get_Details_of_PresentsList to consult a Presents List

---
+++ MyPresentLists_Client/src/main/java/be/walbert/servlets/Get_Details_of_PresentsList.java
@@ -0,0 +1,40 @@
+package be.walbert.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import be.walbert.javabeans.Presents_List;
+
+public class Get_Details_of_PresentsList extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public Get_Details_of_PresentsList() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {	        
+	        Presents_List presents_list = new Presents_List();
+	        presents_list.setId_list(Integer.parseInt(request.getParameter("id")));
+	        
+	        presents_list = presents_list.find();
+	        
+	        if(presents_list != null) {
+	        	
+	        	 request.setAttribute("presents_list", presents_list);
+	             getServletContext().getRequestDispatcher("/WEB-INF/Get_Details_of_PresentsList.jsp").forward(request, response);
+	         }
+		} catch (Exception e) {
+			getServletContext().getRequestDispatcher("/WEB-INF/Error.jsp").forward(request, response);
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
