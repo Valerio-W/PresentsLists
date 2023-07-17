0a5f596f24961216566a8a957dd0691951e9164d
Valerio-W <valerio.walbert@outlook.com>
2023-07-17 22:45:25 +0200

    Update all lists each time a user logs in

--- MyPresentLists_Client/src/main/java/be/walbert/servlets/Login.java
+++ MyPresentLists_Client/src/main/java/be/walbert/servlets/Login.java
@@ -1,6 +1,7 @@
 package be.walbert.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,30 +11,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import be.walbert.javabeans.Presents_List;
 import be.walbert.javabeans.Users;
 
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public Login() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/Login.jsp");
 	    dispatcher.forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		 String pseudo = request.getParameter("pseudo");
 	     String password = request.getParameter("password");
@@ -47,6 +40,10 @@ public class Login extends HttpServlet {
 	    }
 	    else {
 	    	request.getSession().setAttribute("user", user);
+	    	ArrayList<Presents_List> all_presentsList = Presents_List.findAllLists();
+	    	for (Presents_List presents_List : all_presentsList) {
+				presents_List.update_PresentsList();
+			}
 	        response.sendRedirect(request.getContextPath() + "/UserPage");
 	    }
 	}
