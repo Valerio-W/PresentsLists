66f59f3c171834d673ca812d038b6ed99dbd22d4
Valerio-W <valerio.walbert@outlook.com>
2023-07-19 11:15:05 +0200

    Implement create method in PresentDAO file (Client) to access to the API

--- MyPresentLists_Client/src/main/java/be/walbert/DAO/PresentDAO.java
+++ MyPresentLists_Client/src/main/java/be/walbert/DAO/PresentDAO.java
@@ -2,14 +2,37 @@ package be.walbert.DAO;
 
 import java.util.ArrayList;
 
+import javax.ws.rs.core.MediaType;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.jersey.api.client.ClientResponse;
+
 import be.walbert.javabeans.Present;
 
 public class PresentDAO extends DAO<Present>{
 
 	@Override
 	public boolean create(Present obj) {
-		// TODO Auto-generated method stub
-		return false;
+		//Convert Object to json
+		ObjectMapper objectMapper = new ObjectMapper();
+
+	    try {
+	        // Send POST request with JSON
+			String json = objectMapper.writeValueAsString(obj);
+	        ClientResponse res = this.ressource
+	                .path("present")
+	                .accept(MediaType.APPLICATION_JSON)
+	                .type(MediaType.APPLICATION_JSON)
+	                .post(ClientResponse.class, json);
+
+	        if (res.getStatus() == 201) {
+	            return true;
+	        }
+	    } catch (Exception ex) {
+	        System.out.println(ex.getMessage());
+	        return false;
+	    }
+	    return false;
 	}
 
 	@Override
