d2513e5ac634c0a4873ea956f0050bce6c764c30
Valerio-W <valerio.walbert@outlook.com>
2023-07-22 13:38:28 +0200

    Implement find meethod in PresentDAO_API to find a present in database

--- MyPresentLists_API/src/main/java/be/walbert/DAO/PresentDAO_API.java
+++ MyPresentLists_API/src/main/java/be/walbert/DAO/PresentDAO_API.java
@@ -1,14 +1,18 @@
 package be.walbert.DAO;
 
 import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import java.sql.Array;
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.sql.Types;
 import java.util.ArrayList;
+import java.util.Base64;
 
 import be.walbert.Javabeans.Present_API;
+import be.walbert.Javabeans.Presents_List_API;
+import oracle.jdbc.OracleTypes;
 
 public class PresentDAO_API extends DAO<Present_API> {
 
@@ -56,14 +60,76 @@ public class PresentDAO_API extends DAO<Present_API> {
 
 	@Override
 	public Present_API find(int id) {
-		// TODO Auto-generated method stub
+		try {
+			CallableStatement callableStatement = connect.prepareCall("{call Find_Present(?, ?)}");
+
+	        callableStatement.setInt(1, id);
+	        callableStatement.registerOutParameter(2, OracleTypes.CURSOR);
+
+	        callableStatement.execute();
+
+	        ResultSet resultSet = (ResultSet) callableStatement.getObject(2);
+
+	        if (resultSet.next()) {
+
+	            int idPresent = resultSet.getInt("ID_PRESENT");
+	            String name = resultSet.getString("NAME");
+	            String description = resultSet.getString("DESCRIPTION");
+	            double average_price = resultSet.getDouble("AVERAGE_PRICE");
+	            int priority = resultSet.getInt("PRIORITY");
+	            String statePresent = resultSet.getString("STATE");
+	            String link = resultSet.getString("LINK");
+	            byte[] image = resultSet.getBytes("IMAGE");
+	            int idList = resultSet.getInt("ID_LIST");
+
+	            Presents_List_API presents_list = Presents_List_API.find(idList);
+	            Present_API present = new Present_API(idPresent, name, description, average_price, priority, statePresent, link, image, presents_list);
+	            return present;
+	        }
+            return null;
+
+		} catch (Exception e) {
+	        e.printStackTrace();
+ 		}
 		return null;
 	}
-
 	@Override
 	public boolean update(Present_API obj) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+            CallableStatement cstmt = connect.prepareCall("{call UpdatePresent(?, ?, ?, ?, ?, ?, ?, ?, ?)}");
+
+            cstmt.setInt(1, obj.getId_present());
+            cstmt.setString(2, obj.getName());
+            cstmt.setString(3, obj.getDescription());
+            cstmt.setDouble(4, obj.getAverage_price());
+            cstmt.setInt(5, obj.getPriority());
+            cstmt.setString(6, obj.getState());
+
+            // Handle the case when link is absent
+            if (obj.getLink() != null && !obj.getLink().isEmpty()) {
+                cstmt.setString(7, obj.getLink());
+            } else {
+                cstmt.setNull(7, java.sql.Types.VARCHAR);
+            }
+
+            // Handle the case when image is absent
+            if (obj.getImage() != null) {
+                ByteArrayInputStream inputStream = new ByteArrayInputStream(obj.getImage());
+                cstmt.setBlob(8, inputStream);
+            } else {
+                cstmt.setNull(8, java.sql.Types.BLOB);
+            }
+
+            cstmt.setInt(9, obj.getList().getId_list());
+
+            cstmt.execute();
+            cstmt.close();
+
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
 	}
 
 	@Override
