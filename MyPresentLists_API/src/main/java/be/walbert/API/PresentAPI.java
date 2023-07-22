3e157c46d5ba33d58603600a6482aa5b048d8f62
Valerio-W <valerio.walbert@outlook.com>
2023-07-22 13:41:56 +0200

    Add "update" endpoint in PresentAPI file to access to the API's files

--- MyPresentLists_API/src/main/java/be/walbert/API/PresentAPI.java
+++ MyPresentLists_API/src/main/java/be/walbert/API/PresentAPI.java
@@ -4,8 +4,11 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.Base64;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -56,5 +59,60 @@ public class PresentAPI {
 	    } catch (Exception ex) {
 	        return Response.status(Status.BAD_REQUEST).build();
 	    }
+	}
+	
+	@Path("/update")
+	@PUT
+	@Produces(MediaType.APPLICATION_JSON)
+ 	public Response updatePresentsList(String json) {
+	    if (json == null) {
+	        return Response.status(Status.BAD_REQUEST).build();
+	    }
+
+	    try {
+	        JSONObject jsonObject = new JSONObject(json);
+
+    	    int id_present = jsonObject.getInt("id_present");
+	        String name = jsonObject.getString("name");
+    	    String description = jsonObject.getString("description");
+    	    double average_price = jsonObject.getDouble("average_price");
+    	    int priority = jsonObject.getInt("priority");
+    	    String state = jsonObject.getString("state");
+    	    String link = jsonObject.getString("link");
+    	    String imageBase64 = jsonObject.getString("image");
+	    	byte[] image = Base64.getDecoder().decode(imageBase64);
+    	    
+    	    JSONObject list_object = jsonObject.getJSONObject("list");
+    	    Presents_List_API list = new Presents_List_API();
+    	    list.setId_list(list_object.getInt("id_list"));
+    	    
+    	    Present_API present = new Present_API(id_present,name,description, average_price,priority , state, link, image, list);
+
+	        if (!present.update()) {
+	            return Response.status(Status.SERVICE_UNAVAILABLE).build();
+	        } else {
+	            return Response.status(Status.OK).build();
+	        }
+	    } catch (Exception ex) {
+	        return Response.status(Status.BAD_REQUEST).build();
+	    }
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("{id}")
+	public Response find_Present(@PathParam("id") int id_present) {
+		Present_API present = new Present_API();
+		try {
+			present = Present_API.find(id_present);
+
+			if (present == null) {
+				return Response.status(Status.SERVICE_UNAVAILABLE).build();
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return Response.status(Status.OK).entity(present).build();
 	}
 }
