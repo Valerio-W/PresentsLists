4b9644f2b41f535ca93a4c450d43e5d8e3f6828c
Valerio-W <valerio.walbert@outlook.com>
2023-07-28 16:23:22 +0200

    Implement the AddGuest servlet with doGet and doPost methods

--- MyPresentLists_Client/src/main/java/be/walbert/servlets/AddGuests.java
+++ MyPresentLists_Client/src/main/java/be/walbert/servlets/AddGuests.java
@@ -6,32 +6,60 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import be.walbert.javabeans.Present;
+import be.walbert.javabeans.Presents_List;
+import be.walbert.javabeans.Users;
 
 public class AddGuests extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public AddGuests() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		try {	        
+	        Presents_List presents_list = new Presents_List();
+	        presents_list.setId_list(Integer.parseInt(request.getParameter("id")));
+	        
+	        presents_list = Presents_List.find(Integer.parseInt(request.getParameter("id")));
+
+	        if(presents_list != null) {
+	        	 // Create new session with id_list
+		        HttpSession newSession = request.getSession(true);
+	        	newSession.setAttribute("list", presents_list);
+	            getServletContext().getRequestDispatcher("/WEB-INF/AddGuest.jsp").forward(request, response);
+	         }
+		} catch (Exception e) {
+			getServletContext().getRequestDispatcher("/WEB-INF/Error.jsp").forward(request, response);
+		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		HttpSession session = request.getSession(false); 
+		if (session != null && session.getAttribute("list") != null) {
+			Presents_List list = (Presents_List) session.getAttribute("list");
+			Users users_existing = new Users();
+			users_existing.setPseudo(request.getParameter("pseudo"));
+			if(users_existing.isUsersExist() != null) {
+				list.addGuest(users_existing.isUsersExist());
+				if(list.update_PresentsList()) {
+					response.getWriter().append("Guest added !");
+				}
+				else {
+					getServletContext().getRequestDispatcher("/WEB-INF/Error.jsp").forward(request, response);
+				}
+
+			}else {
+				request.getSession().setAttribute("errorUsersNotFound", "Sorry, no Users found with this pseudo");
+	            response.sendRedirect(request.getContextPath() + "/AddGuests"+"?id="+list.getId_list());
+			}
+		}
+		else {
+			getServletContext().getRequestDispatcher("/WEB-INF/Error.jsp").forward(request, response);
+		}
 	}
 
 }
---
+++ MyPresentLists_Client/src/main/webapp/WEB-INF/AddGuest.jsp
@@ -0,0 +1,37 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Add Guests</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="./assets/favicon.ico" />
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="./css/styles.css" rel="stylesheet" />
+</head>
+	<body class="Pages">
+		<h1>Add Guests on Presents List</h1>
+		<form class="Form" action="AddGuests" method="POST">
+	 	<div class="row align-items-stretch mb-5">     
+        	<h3>New Guest</h3>
+        	<div class="col-md-6"> 
+           		<div class="form-group">
+                	<!-- Name input-->
+                	<input name= "pseudo" class="form-control" id="pseudo" type="text" placeholder="Pseudo *" required="required" />
+                </div> 
+        	</div>                       
+        </div>
+        <!-- Submit Button-->
+		<button class="btn btn-primary btn-xl text-uppercase" id="submit" name="submit" type="submit">Add</button>
+     </form>
+	</body>
+</html>
